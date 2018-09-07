f0868325e8d8ae4f79afda4954a508005d6a2c23
mjaylower <mlower@students.mchenry.edu>
2018-09-07 09:25:14 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] strings = {"Hot Dog", "Hamburger", "Brat", "Pulled Pork", "Salad"};
+        for (String menu : strings) {
+            System.out.println(menu);
+        }
+    }
+}
